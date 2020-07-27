e4e72551146fe7df92446fc2aa36c38221514485
zkj <438101845@qq.com>
2020-07-27 19:13:53 +0800

    [zkj]-feat: finish boolean_spec.js

--- __tests__/pieceOfCake/boolean_spec.js
+++ __tests__/pieceOfCake/boolean_spec.js
@@ -10,7 +10,7 @@ describe('for boolean type', () => {
 
     // <--start
     // Please write down the correct value. You should write the final result directly.
-    const expected = undefined;
+    const expected = [true, true, true, true, false, false, false, true];
     // --end->
 
     expect(actual).toEqual(expected);
@@ -27,7 +27,7 @@ describe('for boolean type', () => {
 
     // <--start
     // Please write down the correct value. You should write the final result directly.
-    const expected = undefined;
+    const expected = [true, false, false, false];
     // --end->
 
     expect(actual).toEqual(expected);
